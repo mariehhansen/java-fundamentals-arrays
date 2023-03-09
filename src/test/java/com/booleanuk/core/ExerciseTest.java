2129d959d22e4f37f6faf51c35b5037d7534bc10
Dave Ames <david.john.ames@gmail.com>
2023-03-09 12:22:12 +0000

    Add the order annotations to make the tests run and display in the correct order.

--- src/test/java/com/booleanuk/core/ExerciseTest.java
+++ src/test/java/com/booleanuk/core/ExerciseTest.java
@@ -1,8 +1,12 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ExerciseTest {
     Exercise exercise;
 
@@ -11,16 +15,19 @@ class ExerciseTest {
     }
 
     @Test
+    @Order(1)
     public void shouldChange17To68() {
         Assertions.assertEquals(68, this.exercise.one()[2]);
     }
 
     @Test
+    @Order(2)
     public void shouldBeCarlo() {
         Assertions.assertEquals("Carlo", this.exercise.two());
     }
 
     @Test
+    @Order(3)
     public void shouldContainCarManufacturers() {
         String[] cars = this.exercise.three();
         Assertions.assertEquals(3, cars.length);
@@ -30,11 +37,13 @@ class ExerciseTest {
     }
 
     @Test
+    @Order(4)
     public void shouldBe163() {
         Assertions.assertEquals(163, this.exercise.four());
     }
 
     @Test
+    @Order(5)
     public void fiveShouldBeArrayOfFloats() {
         float[] floats = this.exercise.five();
         Assertions.assertEquals(3, floats.length);
