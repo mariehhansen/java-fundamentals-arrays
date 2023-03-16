40f8d9594a2c657e65a6ba50b991ed60f95de16e
Dave Ames <david.john.ames@gmail.com>
2023-03-16 10:04:36 +0000

    Revert "Add the order annotations to make the tests run and display in the correct order."
    This reverts commit 2129d959d22e4f37f6faf51c35b5037d7534bc10.

--- src/test/java/com/booleanuk/core/ExerciseTest.java
+++ src/test/java/com/booleanuk/core/ExerciseTest.java
@@ -1,12 +1,8 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ExerciseTest {
     Exercise exercise;
 
@@ -15,19 +11,16 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(1)
     public void shouldChange17To68() {
         Assertions.assertEquals(68, this.exercise.one()[2]);
     }
 
     @Test
-    @Order(2)
     public void shouldBeCarlo() {
         Assertions.assertEquals("Carlo", this.exercise.two());
     }
 
     @Test
-    @Order(3)
     public void shouldContainCarManufacturers() {
         String[] cars = this.exercise.three();
         Assertions.assertEquals(3, cars.length);
@@ -37,13 +30,11 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(4)
     public void shouldBe163() {
         Assertions.assertEquals(163, this.exercise.four());
     }
 
     @Test
-    @Order(5)
     public void fiveShouldBeArrayOfFloats() {
         float[] floats = this.exercise.five();
         Assertions.assertEquals(3, floats.length);
